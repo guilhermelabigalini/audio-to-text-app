79eefac829cb1bcab5676eaf6286521f68e7f2ae
Guilherme Labigalini <guilherme.labigalini@gmail.com>
2018-03-18 10:13:22 -0300

    backup

--- backend/AudioToTextService/AudioToTextService.Web/Controllers/HomeController.cs
+++ backend/AudioToTextService/AudioToTextService.Web/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Web;
 using System.Web.Mvc;
 
@@ -10,9 +11,27 @@ namespace AudioToTextService.Web.Controllers
     {
         public ActionResult Index()
         {
+            var a = System.Reflection.Assembly.GetExecutingAssembly();
+
+            ViewBag.Version = a.GetName().Version;
             ViewBag.Title = "Home Page";
 
             return View();
         }
+
+        public ActionResult Partial()
+        {
+            for (int i = 1; i < 10; i++)
+            {
+
+                Response.Write("<br>" + i);
+                Response.Flush();
+
+                Thread.Sleep(2000);
+            }
+
+            return null;
+        }
+
     }
 }
